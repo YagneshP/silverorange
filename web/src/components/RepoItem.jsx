9d667d423eb764b69f398bea6a34369d6d37c125
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-07 06:28:18 -0400

    Add css style to components

--- web/src/components/RepoItem.jsx
+++ web/src/components/RepoItem.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const RepoItem = ({ repo }) => {
-  return <div>{repo.name}</div>;
+  return (
+    <li className="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+      <a href={repo.html_url}>{repo.name}</a>
+    </li>
+  );
 };
 
 export default RepoItem;
--- web/src/components/RepoList.jsx
+++ web/src/components/RepoList.jsx
@@ -5,7 +5,9 @@ const RepoList = ({ repos }) => {
   const repoList = repos
     ? repos.map((repo) => <RepoItem key={repo.name} repo={repo} />)
     : 'Nothing to display';
-  return <div className="text-center">{repoList}</div>;
+  return (
+    <div className="flex flex-col items-start p-3 space-y-3">{repoList}</div>
+  );
 };
 
 export default RepoList;
