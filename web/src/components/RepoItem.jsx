29fbc3973059fcb744ff2489ca0038d8fb0575c6
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-08 07:23:22 -0400

    Add fields to display

--- web/src/components/RepoItem.jsx
+++ web/src/components/RepoItem.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 const RepoItem = ({ repo }) => {
   return (
-    <li className="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-      <a href={repo.html_url}>{repo.name}</a>
+    <li className="flex flex-col p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+      <h3>
+        <span>Name : </span>
+        <a href={repo.html_url}>{repo.name}</a>
+      </h3>
+      <p>
+        <span>Description : </span>{' '}
+        {repo.description ? repo.description : 'not provided'}
+      </p>
+      <p>
+        <span>Language : </span> {repo.language}
+      </p>
+      <p>
+        <span>Fork Count : </span> {repo.forks_count}
+      </p>
+      <p>
+        <span>Created At : </span>
+        {repo.created_at}
+      </p>
     </li>
   );
 };
