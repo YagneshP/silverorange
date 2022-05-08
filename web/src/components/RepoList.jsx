263908a00afcb6334ea11ebc7541b4de38275ba3
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-08 09:00:20 -0400

    Filter repos according to language

--- web/src/components/RepoList.jsx
+++ web/src/components/RepoList.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 import RepoItem from './RepoItem';
 
 const RepoList = ({ repos }) => {
-  const repoList = repos
-    ? repos.map((repo) => <RepoItem key={repo.name} repo={repo} />)
+  const [languageFilter, setLanguageFilter] = React.useState(null);
+  const [filteredRepos, setFilteredRepos] = React.useState(null);
+  React.useEffect(() => {
+    if (repos) {
+      setFilteredRepos(repos);
+    }
+  }, [repos]);
+  React.useEffect(() => {
+    // filter repos according to language
+    if (languageFilter && repos) {
+      const filtered = repos.filter((repo) => repo.language === languageFilter);
+      setFilteredRepos(filtered);
+    }
+  }, [languageFilter, repos]);
+  const repoList = filteredRepos
+    ? filteredRepos.map((repo) => (
+        <RepoItem
+          key={repo.name}
+          repo={repo}
+          setLanguageFilter={setLanguageFilter}
+        />
+      ))
     : 'Nothing to display';
   return (
     <div className="flex flex-col items-start p-3 space-y-3">{repoList}</div>
