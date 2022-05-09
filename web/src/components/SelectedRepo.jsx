b5a012f781c2691bfbef69b4f9727183b3fd20bd
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-08 22:57:27 -0400

    Add style

--- web/src/components/SelectedRepo.jsx
+++ web/src/components/SelectedRepo.jsx
@@ -22,12 +22,38 @@ const SelectedRepo = ({ repo, setSelectedRepo }) => {
       .catch((err) => console.log('err', err));
   }, [repo]);
   return (
-    <section>
-      <h2>Author : {recentCommit?.commit.author.name}</h2>
-      <p>Message : {recentCommit?.commit.message}</p>
-      <p>Date : {recentCommit?.commit.author.date}</p>
-      <button onClick={() => setSelectedRepo(null)}> Back </button>
-      <div>{markdown}</div>
+    <section className="container mx-auto mt-2">
+      <div className="flex justify-evenly">
+        <div>
+          <h2 className="text-gray-500 font-medium ">
+            <span className="text-black font-bold">Author : </span>
+            {recentCommit?.commit.author.name}
+          </h2>
+          <p className="text-gray-500 font-medium ">
+            <span className="text-black font-bold">Message :</span>{' '}
+            {recentCommit?.commit.message}
+          </p>
+          <p className="text-gray-500 font-medium ">
+            <span className="text-black font-bold">Date :</span>{' '}
+            {recentCommit?.commit.author.date}
+          </p>
+        </div>
+        <button
+          className="border rounded-full px-4 h-10 bg-sky-500 text-white"
+          onClick={() => setSelectedRepo(null)}
+        >
+          Back
+        </button>
+      </div>
+
+      {markdown && (
+        <div className="flex flex-col items-center">
+          <h1 className="text-black font-bold text-2xl py-4">
+            Markdown Content
+          </h1>
+          <p>{markdown}</p>
+        </div>
+      )}
     </section>
   );
 };
