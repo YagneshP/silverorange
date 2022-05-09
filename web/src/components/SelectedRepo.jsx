9a58f8e6dd3db4ab829c8a216a22ffa2b306427e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-08 22:21:52 -0400

    Fetch the markdown content for repo

--- web/src/components/SelectedRepo.jsx
+++ web/src/components/SelectedRepo.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 const SelectedRepo = ({ repo, setSelectedRepo }) => {
   const [recentCommit, setRecentCommit] = React.useState(null);
+  const [markdown, setMarkdown] = React.useState(null);
+
   React.useEffect(() => {
     axios(
       `https://api.github.com/repos/${repo.owner.login}/${repo.name}/commits/master`
@@ -11,12 +13,21 @@ const SelectedRepo = ({ repo, setSelectedRepo }) => {
       .catch((err) => console.log('err', err));
   }, [repo]);
 
+  React.useEffect(() => {
+    axios(
+      `https://raw.githubusercontent.com/${repo.full_name}/master/README.md`
+      // `https://api.github.com/repos/${repo.owner.login}/${repo.name}/commits/master`
+    )
+      .then((res) => setMarkdown(res.data))
+      .catch((err) => console.log('err', err));
+  }, [repo]);
   return (
     <section>
       <h2>Author : {recentCommit?.commit.author.name}</h2>
       <p>Message : {recentCommit?.commit.message}</p>
       <p>Date : {recentCommit?.commit.author.date}</p>
       <button onClick={() => setSelectedRepo(null)}> Back </button>
+      <div>{markdown}</div>
     </section>
   );
 };
